74fefdfbb36426a54786eea486ad21e1f21bdc63
crosbourne <neilcrosbourne@hotmail.com>
2015-02-12 21:34:26 +0000

    browse search fixed, started slide page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,25 +27,27 @@ class PagesController < ApplicationController
   end
 
   def search
-    if !params[:movies].nil? && params['search'].empty?
-      @results = @movies = ComicVine::API.movies({limit: 20})
+    if !params[:movies].nil? && !params['search'].empty?
+      @results = @movies = ComicVine::API.movies({limit: 20, filter: "name:#{params[:search]}"})
    
-    elsif !params[:characters].nil? && params['search'].empty?
-      @results = @characters = ComicVine::API.characters({limit: 20})
+    elsif !params[:characters].nil? && !params['search'].empty?
+      @results = @characters = ComicVine::API.characters({limit: 20, filter: "name:#{params[:search]}"})
   
-    elsif !params[:issues].nil? && params['search'].empty?
-      @results = @issues = ComicVine::API.issues({limit: 20})
+    elsif !params[:issues].nil? && !params['search'].empty?
+      @results = @issues = ComicVine::API.issues({limit: 20, 
+        filter: "description:#{params[:search]},issue_number:#{params[:search]}"})
     
-    elsif !params[:volume].nil? && params['search'].empty?
-      @results = @volumes = ComicVine::API.volumes({limit: 20})
+    elsif !params[:volumes].nil? && !params['search'].empty?
+      @results = @volumes = ComicVine::API.volumes({limit: 20, 
+        filter: "description:#{params[:search]},deck:#{params[:search]},name:#{params[:search]}"})
     
     else 
       terms = [params[:movies],params[:characters],params[:issues],params[:volumes]].compact.join(",")
 
       @results = ComicVine::API.search terms, params['search'], {:limit => 20}
-      render :browse
-      #raise
+
     end
+    render :browse
     
   end
 
