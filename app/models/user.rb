6e0cffef9c2756d3138c62cd3d80d60e458b3a1d
crosbourne <neilcrosbourne@hotmail.com>
2015-02-13 14:33:52 +0000

    added google login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :albums, allow_destroy: :false
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable,:omniauthable, omniauth_providers: [:facebook]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable,:omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   mount_uploader :user_avatar, UserAvatarUploader
 
   before_save :set_default_user_role
