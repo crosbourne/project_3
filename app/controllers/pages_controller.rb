88ac136794fe867fcdb9972a6b6e1ef5c9dce575
crosbourne <neilcrosbourne@hotmail.com>
2015-02-20 14:09:02 +0000

    search box fixed on browse

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -36,21 +36,27 @@ class PagesController < ApplicationController
 
   def search
     if !params[:movies].nil? && !params['search'].empty?
+      
+
       @results = @movies = (ComicVine::API.movies({limit: 20, filter: URI.escape("name:#{params[:search]}")}) rescue [])
    
     elsif !params[:characters].nil? && !params['search'].empty?
+      
+
       @results = @characters = (ComicVine::API.characters({limit: 20, filter: URI.escape("name:#{params[:search]}")})rescue [])
       
     
     elsif !params[:volumes].nil? && !params['search'].empty?
+  
+
       @results = @volumes = (ComicVine::API.volumes({limit: 20, 
         filter: URI.escape("description:#{params[:search]},deck:#{params[:search]},name:#{params[:search]}")}) rescue [])
     
     else 
-      terms = [params[:movies],params[:characters],params[:issues],params[:volumes]].compact.join(",")
-
-      @results = ComicVine::API.search terms, params['search'], {:limit => 40}
-
+      terms = [params[:movies],params[:characters],params[:volumes]].compact.join(",")
+      if terms.present? && params[:search].present?
+        @results = ComicVine::API.search terms, params['search'], {:limit => 40}
+      end
     end
     render :browse
     
